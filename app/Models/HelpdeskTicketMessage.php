e941c55c9b2063f52020babd7033a90aa77f019f
Kenneth Hansen <kenneth@vkdata.dk>
2020-01-08 08:32:08 +0100

    Fix HTML display

--- app/Models/HelpdeskTicketMessage.php
+++ app/Models/HelpdeskTicketMessage.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\HelpdeskTicket;
 use App\Models\HelpdeskTicketMessageAttachment;
 use App\User;
+use DOMDocument;
+use DOMXPath;
 
 class HelpdeskTicketMessage extends Model
 {
@@ -38,5 +40,14 @@ class HelpdeskTicketMessage extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function viewHTML()
+    {
+        $dom = new DOMDocument();
+        $dom->loadHTML($this->html);
+        $xpath = new DOMXPath($dom);
+        $body = $xpath->query("//body")[0]->nodeValue;
+        return $body;
     }
 }
